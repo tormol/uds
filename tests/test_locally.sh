572ee008bca358a12f0cc078d0ba42eb276abe74
Torbjørn Birch Moltu <t.b.moltu@lyse.net>
2021-01-18 23:52:47 +0100

    Slow down test_locally a bit
    Today it was rustc's turn to make my laptop overheat and power off.
    (yeah I need to clean some dust.)
    
    Also try to log commands (but it logs loop bodies instead)
    and add minimal-versions check with mio_07.

--- tests/test_locally.sh
+++ tests/test_locally.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 MSRV="1.39.0"
 
-set -e
+set -ev
 export RUST_BACKTRACE=1
+
+CAFLAGS="-j1"
 
 check_targets="x86_64-unknown-freebsd x86_64-unknown-netbsd \
                x86_64-apple-darwin x86_64-sun-solaris \
@@ -11,15 +13,15 @@ check_targets="x86_64-unknown-freebsd x86_64-unknown-netbsd \
 # not available: dragonfly, openbsd and illumos
 for target in $check_targets; do
     echo "checking $target"
-    cargo check --target "$target" --tests --examples
-    cargo check --target "$target" --tests --examples --features mio
-    cargo check --target "$target" --tests --examples --features mio-uds
-    cargo check --target "$target" --features mio_07
-    cargo check --target "$target" --features tokio
-    cargo check --target "$target" --all-features
-    RUSTFLAGS='--cfg features="os-poll"' cargo check --target "$target" --tests --examples --features mio_07
-    RUSTFLAGS='--cfg features="os-poll"' cargo check --target "$target" --tests --examples --features tokio
-    RUSTFLAGS='--cfg features="os-poll"' cargo check --target "$target" --tests --examples --all-features
+    cargo check $CAFLAGS --target "$target" --tests --examples
+    cargo check $CAFLAGS --target "$target" --tests --examples --features mio
+    cargo check $CAFLAGS --target "$target" --tests --examples --features mio-uds
+    cargo check $CAFLAGS --target "$target" --features mio_07
+    cargo check $CAFLAGS --target "$target" --features tokio
+    cargo check $CAFLAGS --target "$target" --all-features
+    RUSTFLAGS='--cfg features="os-poll"' cargo check $CAFLAGS --target "$target" --tests --examples --features mio_07
+    RUSTFLAGS='--cfg features="os-poll"' cargo check $CAFLAGS --target "$target" --tests --examples --features tokio
+    RUSTFLAGS='--cfg features="os-poll"' cargo check $CAFLAGS --target "$target" --tests --examples --all-features
     echo
 done
 
@@ -27,8 +29,8 @@ test_targets="x86_64-unknown-linux-gnu x86_64-unknown-linux-musl \
               i686-unknown-linux-gnu i686-unknown-linux-musl"
 for target in $test_targets; do
     echo "testing $target"
-    cargo check --target "$target" --all-features
-    RUSTFLAGS='--cfg features="os-poll"' cargo test --target "$target" --all-features -- --quiet
+    cargo check $CAFLAGS --target "$target" --all-features
+    RUSTFLAGS='--cfg features="os-poll"' cargo $CAFLAGS test --target "$target" --all-features -- --quiet
     echo
 done
 
@@ -36,21 +38,22 @@ export RUSTFLAGS='--cfg features="os-poll"'
 
 test_release_target="x86_64-unknown-linux-gnux32" # segfaults in debug mode
 echo "testing $test_release_target (in release mode)"
-cargo test --target "$test_release_target" --release --all-features -- --quiet
+cargo test $CAFLAGS --target "$test_release_target" --release --all-features -- --quiet
 echo
 
 echo "checking with minimum supported Rust version $MSRV"
 rm Cargo.lock
-cargo "+$MSRV" check --all-features --tests --examples
+cargo "+$MSRV" check $CAFLAGS --all-features --tests --examples
 # requiring stable for tests or examples is OK though,
 # just remove the option then.
 echo
 
 echo "checking with minimum version dependencies"
 rm Cargo.lock
-cargo +nightly check -Z minimal-versions
-cargo +nightly check -Z minimal-versions --features mio
+cargo +nightly check $CAFLAGS -Z minimal-versions
+cargo +nightly check $CAFLAGS -Z minimal-versions --features mio
 # no version of mio-uds compiles with minimal versions
+cargo +nightly check $CAFLAGS -Z minimal-versions --features mio_07
 rm Cargo.lock
 echo
 

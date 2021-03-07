3926410bbf475f269a6f6b21dadf32ee1cdb80ac
Torbjørn Birch Moltu <t.b.moltu@lyse.net>
2021-03-07 12:32:26 +0100

    Re-enable local x86_64-linux-x32 test
    With nightly instead of release mode requirement.
    (LLVM 12 fixed the bugs).

--- tests/test_locally.sh
+++ tests/test_locally.sh
@@ -36,10 +36,10 @@ done
 
 export RUSTFLAGS='--cfg feature="os-poll"'
 
-#test_release_target="x86_64-unknown-linux-gnux32" # segfaults in debug mode (and release mode too now)
-#echo "testing $test_release_target (in release mode)"
-#cargo test $CAFLAGS --target "$test_release_target" --release --all-features -- --quiet
-#echo
+test_nightly_target="x86_64-unknown-linux-gnux32" # segfaults fixed with LLVM 12
+echo "testing $test_nightly_target (on nightly)"
+RUSTFLAGS='--cfg feature="os-poll"'  cargo +nightly test $CAFLAGS --target "$test_nightly_target" --release --all-features -- --quiet
+echo
 
 echo "checking with minimum supported Rust version $MSRV"
 rm Cargo.lock
